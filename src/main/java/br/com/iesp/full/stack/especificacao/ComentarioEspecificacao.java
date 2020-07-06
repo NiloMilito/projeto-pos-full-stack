80e4071b6a1cb3c234ccf63304a8d0b6bb92b44d
Danilo Pereira da Costa da Silva <nilomilito@gmail.com>
2020-07-06 00:16:22 -0300

    Aplicando Specification

---
+++ src/main/java/br/com/iesp/full/stack/especificacao/ComentarioEspecificacao.java
@@ -0,0 +1,36 @@
+package br.com.iesp.full.stack.especificacao;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.data.jpa.domain.Specification;
+import br.com.iesp.full.stack.entidades.Comentario;
+
+public class ComentarioEspecificacao implements Specification<Comentario> {
+
+	private static final long serialVersionUID = 1L;
+	private Comentario filtro;
+		
+	public ComentarioEspecificacao(Comentario filtro) {
+		super();
+		this.filtro = filtro;
+	}
+
+	@Override
+	public Predicate toPredicate(Root<Comentario> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+		Predicate predicate = criteriaBuilder.disjunction();
+
+		if (filtro.getDescricao() != null) {
+			predicate.getExpressions().add(criteriaBuilder.like(criteriaBuilder.upper(root.get("descricao")), "%"+ filtro.getDescricao().toUpperCase() +"%"));
+		}
+
+		if (filtro.getData() != null) {
+			predicate.getExpressions().add(criteriaBuilder.and(criteriaBuilder.equal(root.get("data"), filtro.getData())));
+		}
+		
+		return predicate;
+	}
+
+}
